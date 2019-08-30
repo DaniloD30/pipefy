e4fc7be6ee10481950ecf2a6705f7327f33c55f4
danilo.dias <danilo.dias@solutis.com.br>
2019-08-30 15:23:38 -0300

    Implemented card drag

--- dodfy/src/App.js
+++ dodfy/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import Header from './componentes/Header';
 import Board from './componentes/Board';
+import { DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 function App() {
   return (
     // fragmento, n aparece dentro do HTML
-    <>
+    <DndProvider backend={HTML5Backend}>
+     
       <Header />
       <Board />
       <GlobalStyle />
-    </>
+    </DndProvider>
     );
 }
 export default App;
--- dodfy/src/componentes/Card/index.js
+++ dodfy/src/componentes/Card/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {Container, Label} from './styles';
-
+import {useDrag} from 'react-dnd';
 export default function Card({ data }){
+  const [{isDragging}, dragRef] = useDrag({
+    item: {type:'CARD'},
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+
+  })
     return(
-           <Container>
+           <Container ref={dragRef} isDragging={isDragging}>
                    <header>
                       {data.labels.map(label => <Label key={label} color={label}/>)}
                    </header>
